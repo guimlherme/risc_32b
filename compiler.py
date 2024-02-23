977f043d474d7c54b1a27b5e062136b4fe01e53e
guimlherme <guilhermeoliveira5001@gmail.com>
2024-02-24 00:45:55 +0100

    Fixed python compiler

--- compiler.py
+++ compiler.py
@@ -3,6 +3,7 @@
 from collections import defaultdict 
 
 funct3_map = {
+    "JALR": "000",
     "BEQ": "000",
     "BNE": "001",
     "BLT": "100",
@@ -68,7 +69,7 @@ for line in Lines:
     if opcode == "":
         continue
     elif opcode[-1] != ":":
-        counter += 1
+        counter += 4
     else:
         gotos[opcode.rstrip(":")] = counter
 
@@ -83,10 +84,16 @@ for line in Lines:
     line = line.strip().rstrip("\n").upper()
     words = line.split(' ')
     words = [w.rstrip().rstrip(',') for w in words]
-    print(words)
     opcode = words[0]
     opcode = opcode.upper()
 
+    if opcode == "":
+        continue
+    if opcode[-1] == ":":
+        assert(gotos[opcode.rstrip(":")] == counter)
+        continue
+    
+    print(words)
     #Pseudoinstructions
     if opcode == "MOV" or opcode == "MV":
         opcode = "ADDI"
@@ -98,100 +105,98 @@ for line in Lines:
     elif opcode == "NOP":
         opcode = "ADDI"
         words = ["ADDI", "x0", "x0", "0"]
+    if '(' in words[2]:
+        words.append((words[2].split('('))[0])
+        words[2] = (words[2].split('('))[1][:-1]
 
-    if opcode == "":
-        continue
-    elif opcode[-1] == ":":
-        assert(gotos[opcode.rstrip(":")] == counter)
-    else:
-        match opcode:
-            case "LUI":
-                rd = int(words[1][1:])
-                imm = int(words[2])
-                imm = int2signedbin(imm, 20)
-                opcode_number = int("0110111", 2)
-                bytecode = '{:020b}{:05b}{:07b}'.format(imm, rd, opcode_number)
-            case "AUIPC":
-                rd = int(words[1][1:])
-                imm = int(words[2][1:])
-                imm = int2signedbin(imm, 20)
-                opcode_number = int("0010111", 2)
-                bytecode = '{:020b}{:05b}{:07b}'.format(imm, rd, opcode_number)
-            case "JAL":
-                rd = int(words[1][1:])
-                imm = int(words[2][1:])
-                imm = int2signedbin(imm, 30)
-                opcode_number = int("0010111", 2)
-                binary_imm = '{:030b}'.format(imm)[::-1]
-                formated_binary_imm = binary_imm[20] + binary_imm[10:0:-1] + binary_imm[11] + binary_imm[19:11:-1]
-                bytecode = '{}{}{}{}{:05b}{:07b}'.format(formated_binary_imm, rd, opcode_number)
-            case "JALR":
-                rd = int(words[1][1:])
-                rs1 = int(words[2][1:])
-                imm = int(words[3])
-                imm = int2signedbin(imm, 12)
-                opcode_number = int("1100111", 2)
-                bytecode = '{:012b}{:05b}{}{:05b}{:07b}'.format(imm, rs1, funct3_map[opcode],rd, opcode_number)
-            case "BEQ"|"BNE"|"BLT"|"BGE"|"BLTU"|"BGEU":
-                rs2 = int(words[1][1:])
-                rs1 = int(words[2][1:])
-                imm = int(words[3])
-                imm = int2signedbin(imm, 30)
-                opcode_number = int("1100011", 2)
-                binary_imm = '{:030b}'.format(imm)[::-1]
-                formated_binary_imm1 = binary_imm[12] + binary_imm[10:4:-1]
-                formated_binary_imm2 = binary_imm[4:0:-1] + binary_imm[11]
-                bytecode = '{}{:05b}{:05b}{}{}{:07b}'.format(formated_binary_imm1, rs2, rs1, funct3_map[opcode], formated_binary_imm2, opcode_number)
-            case "LB"|"LH"|"LW"|"LBU"|"LHU":
-                rd = int(words[1][1:])
-                rs1 = int(words[2][1:])
-                imm = int(words[3])
-                imm = int2signedbin(imm, 12)
-                opcode_number = int("0000011", 2)
-                bytecode = '{:012b}{:05b}{}{:05b}{:07b}'.format(imm, rs1, funct3_map[opcode], rd, opcode_number)
-            case "SB"|"SH"|"SW":
-                rs2 = int(words[1][1:])
-                rs1 = int(words[2][1:])
-                imm = int(words[3])
-                imm = int2signedbin(imm, 12)
-                opcode_number = int("0100011", 2)
-                binary_imm = '{:012b}'.format(imm)[::-1]
-                formated_binary_imm1 =  binary_imm[11:4:-1]
-                formated_binary_imm2 = binary_imm[4:-1:-1]
-                bytecode = '{}{:05b}{:05b}{}{}{:07b}'.format(formated_binary_imm1, rs2, rs1, funct3_map[opcode], formated_binary_imm2, opcode_number)
-            case "ADDI"|"SLTI"|"SLTIU"|"XORI"|"ORI"|"ANDI":
-                rd = int(words[1][1:])
-                rs1 = int(words[2][1:])
-                imm = int(words[3])
-                imm = int2signedbin(imm, 12)
-                opcode_number = int("0010011", 2)
-                bytecode = '{:012b}{:05b}{}{:05b}{:07b}'.format(imm, rs1, funct3_map[opcode], rd, opcode_number)
-            case "SLLI"|"SRLI"|"SRAI":
-                rd = int(words[1][1:])
-                rs1 = int(words[2][1:])
-                imm = int(words[3])
-                assert (imm >= 0)
-                imm = int2signedbin(imm, 5)
-                opcode_number = int("0010011", 2)
-                bytecode = '{}{:05b}{:05b}{}{:05b}{:07b}'.format(funct7_map[opcode], imm, rs1, funct3_map[opcode], rd, opcode_number)
-            case "ADD"|"SUB"|"SLL"|"SLT"|"SLTU"|"XOR"|"SRL"|"SRA"|"OR"|"AND":
-                rd = int(words[1][1:])
-                rs2 = int(words[2][1:])
-                rs1 = int(words[3][1:])
-                opcode_number = int("0110011", 2)
-                bytecode = '{}{:05b}{:05b}{}{:05b}{:07b}'.format(funct7_map[opcode], rs2, rs1, funct3_map[opcode], rd, opcode_number)
-                
+    match opcode:
+        case "LUI":
+            rd = int(words[1][1:])
+            imm = int(words[2])
+            imm = int2signedbin(imm, 20)
+            opcode_number = int("0110111", 2)
+            bytecode = '{:020b}{:05b}{:07b}'.format(imm, rd, opcode_number)
+        case "AUIPC":
+            rd = int(words[1][1:])
+            imm = int(words[2])
+            imm = int2signedbin(imm, 20)
+            opcode_number = int("0010111", 2)
+            bytecode = '{:020b}{:05b}{:07b}'.format(imm, rd, opcode_number)
+        case "JAL":
+            rd = int(words[1][1:])
+            imm = int(words[2])
+            imm = int2signedbin(imm, 30)
+            opcode_number = int("1101111", 2)
+            binary_imm = '{:030b}'.format(imm)[::-1]
+            formated_binary_imm = binary_imm[20] + binary_imm[10:0:-1] + binary_imm[11] + binary_imm[19:11:-1]
+            bytecode = '{}{:05b}{:07b}'.format(formated_binary_imm, rd, opcode_number)
+        case "JALR":
+            rd = int(words[1][1:])
+            rs1 = int(words[2][1:])
+            imm = int(words[3])
+            imm = int2signedbin(imm, 12)
+            opcode_number = int("1100111", 2)
+            bytecode = '{:012b}{:05b}{}{:05b}{:07b}'.format(imm, rs1, funct3_map[opcode],rd, opcode_number)
+        case "BEQ"|"BNE"|"BLT"|"BGE"|"BLTU"|"BGEU":
+            rs2 = int(words[1][1:])
+            rs1 = int(words[2][1:])
+            imm = int(words[3])
+            imm = int2signedbin(imm, 30)
+            opcode_number = int("1100011", 2)
+            binary_imm = '{:030b}'.format(imm)[::-1]
+            formated_binary_imm1 = binary_imm[12] + binary_imm[10:4:-1]
+            formated_binary_imm2 = binary_imm[4:0:-1] + binary_imm[11]
+            bytecode = '{}{:05b}{:05b}{}{}{:07b}'.format(formated_binary_imm1, rs2, rs1, funct3_map[opcode], formated_binary_imm2, opcode_number)
+        case "LB"|"LH"|"LW"|"LBU"|"LHU":
+            rd = int(words[1][1:])
+            rs1 = int(words[2][1:])
+            imm = int(words[3])
+            imm = int2signedbin(imm, 12)
+            opcode_number = int("0000011", 2)
+            bytecode = '{:012b}{:05b}{}{:05b}{:07b}'.format(imm, rs1, funct3_map[opcode], rd, opcode_number)
+        case "SB"|"SH"|"SW":
+            rs2 = int(words[1][1:])
+            rs1 = int(words[2][1:])
+            imm = int(words[3])
+            imm = int2signedbin(imm, 12)
+            opcode_number = int("0100011", 2)
+            binary_imm = '{:012b}'.format(imm)[::-1]
+            formated_binary_imm1 =  binary_imm[11:4:-1]
+            formated_binary_imm2 = binary_imm[4::-1]
+            bytecode = '{}{:05b}{:05b}{}{}{:07b}'.format(formated_binary_imm1, rs2, rs1, funct3_map[opcode], formated_binary_imm2, opcode_number)
+        case "ADDI"|"SLTI"|"SLTIU"|"XORI"|"ORI"|"ANDI":
+            rd = int(words[1][1:])
+            rs1 = int(words[2][1:])
+            imm = int(words[3])
+            imm = int2signedbin(imm, 12)
+            opcode_number = int("0010011", 2)
+            bytecode = '{:012b}{:05b}{}{:05b}{:07b}'.format(imm, rs1, funct3_map[opcode], rd, opcode_number)
+        case "SLLI"|"SRLI"|"SRAI":
+            rd = int(words[1][1:])
+            rs1 = int(words[2][1:])
+            imm = int(words[3])
+            assert (imm >= 0)
+            imm = int2signedbin(imm, 5)
+            opcode_number = int("0010011", 2)
+            bytecode = '{}{:05b}{:05b}{}{:05b}{:07b}'.format(funct7_map[opcode], imm, rs1, funct3_map[opcode], rd, opcode_number)
+        case "ADD"|"SUB"|"SLL"|"SLT"|"SLTU"|"XOR"|"SRL"|"SRA"|"OR"|"AND":
+            rd = int(words[1][1:])
+            rs2 = int(words[2][1:])
+            rs1 = int(words[3][1:])
+            opcode_number = int("0110011", 2)
+            bytecode = '{}{:05b}{:05b}{}{:05b}{:07b}'.format(funct7_map[opcode], rs2, rs1, funct3_map[opcode], rd, opcode_number)
+            
 
-
-        assert (len(bytecode) == 32)
-        file_w.write('Data_Rom({:d}) <= \"{}\";\n'.format(counter,bytecode[0:8]))
-        counter += 1
-        file_w.write('Data_Rom({:d}) <= \"{}\";\n'.format(counter,bytecode[8:16]))
-        counter += 1
-        file_w.write('Data_Rom({:d}) <= \"{}\";\n'.format(counter,bytecode[16:24]))
-        counter += 1
-        file_w.write('Data_Rom({:d}) <= \"{}\";\n'.format(counter,bytecode[24:32]))
-        counter += 1
+    print(bytecode)
+    assert (len(bytecode) == 32)
+    file_w.write('Data_Rom({:d}) <= \"{}\";\n'.format(counter,bytecode[24:32]))
+    counter += 1
+    file_w.write('Data_Rom({:d}) <= \"{}\";\n'.format(counter,bytecode[16:24]))
+    counter += 1
+    file_w.write('Data_Rom({:d}) <= \"{}\";\n'.format(counter,bytecode[8:16]))
+    counter += 1
+    file_w.write('Data_Rom({:d}) <= \"{}\";\n'.format(counter,bytecode[0:8]))
+    counter += 1
 
 
 
